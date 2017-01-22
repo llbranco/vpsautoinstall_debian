168741682ac443aa5bc68eb880f2f33629243c73
Luciano Branco <thelucianobranco@gmail.com>
2017-01-22 16:27:06 -0200

    Update add-user.sh

--- add-user.sh
+++ add-user.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 echo "-----------------------------------"
-echo "  REGISTRO TUNN3LLOVE  "
+echo "  REGISTRO DE USUARIO  "
 echo "-----------------------------------"
 echo -n "Username: "
-read account
+read acc
 echo -n "Senha: "
 read pass
 echo -n "Validade em dias: "
 read exp
 IP=`ifconfig eth0| awk 'NR==2 {print $2}'| awk -F: '{print $2}'`
-useradd -e `date -d "$exp days" +"%Y-%m-%d"` -s /bin/false -M -g clients $account
-echo -e "$pass\n$pass\n"|passwd $account &> /dev/null
+useradd -e `date -d "$exp days" +"%Y-%m-%d"` -s /bin/false -M -g clients $acc
+echo -e "$pass\n$pass\n"|passwd $acc &> /dev/null
 echo "-----------------------------------------------------"
 echo "         SUCESSO           "
 echo "-----------------------------------------------------"
-echo -e "Username: $account "
+echo -e "Username: $acc "
 echo -e "Senha: $pass"
 echo -e "Válido por: $exp dias"
 echo -e "IP: $IP "
